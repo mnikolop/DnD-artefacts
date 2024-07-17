39f640091640e819a77a440e3c57f4137761e606
mnikolop <marknikolop@gmail.com>
2024-07-18 01:37:01 +0200

    removing cache

--- python/dash.py
+++ python/dash.py
@@ -23,14 +23,6 @@ monsters = monsters.rename(columns={"Armor Class": "AC",
 
 
 
-@st.cache_data
-def load_data():
-    data = monsters
-    return data
-# 
-# data_load_state = st.text('Loading data...')
-# data = load_data(10000)
-# data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
